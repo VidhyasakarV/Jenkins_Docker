3c8435d7ba56665a4a34e8ca51f9b29b2f0f06f7
vidhyasakarV <vidhudanzz@gmail.com>
2023-07-14 15:09:02 +0530

    Made changes in api.

--- src/main/java/com/docker_jenkins/dj/DjApplication.java
+++ src/main/java/com/docker_jenkins/dj/DjApplication.java
@@ -2,8 +2,6 @@ package com.docker_jenkins.dj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class DjApplication {
